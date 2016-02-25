fef3ee1ff9a6eba4c8d0aa752a3caa03f3f42efd
Nadine <nlcurry@gmail.com>
2016-02-25 14:51:18 -0800

    Added methods to randomly generate a word and another method to output the image according to how many wrong guesses there are

--- word_guess.rb
+++ word_guess.rb
@@ -1,5 +1,30 @@
 #word guess game
 
 class word_guess
+	def initialize
+		@wrong_count = 0
+
+	def word_gen
+		word_list = ["ghost","pacman",""]
+
+	#asks user for a letter to guess in the word
+	def user_input 
+		puts "Please pick a letter to guess the word."
+		@letter = gets.chomp.downcase
+	end
+
+	def image
+		puts "================================="
+		puts "     .-.  #{l[wrong_count]}  .--.  "
+		puts "    | OO| #{l[wrong_count]} / _.-' "
+		puts "    |   |  o  o  o  o  o  \  '-. "
+		puts "    '^^^' #{l[wrong_count]}  '--'  "
+		puts ""
+	end
+
+l = ["               ", "            ", "         ","      ","   "] 
+dots = []
+	private
+	def guesses
 
 end
